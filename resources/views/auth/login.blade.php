e40d78d45dc71402aed87790f21da3bf2898c186
starite paschal <paschalnwokeocha@gmail.com>
2024-05-17 22:52:41 +0100

    basketball continue

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,72 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+@extends('layouts.ui')
+@section('content')
+<!--// Main Content \\-->
+<div class="sportsmagazine-main-content">
+
+    <!--// Main Section \\-->
+    <div class="sportsmagazine-main-section sportsmagazine-login-form-full">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-12">
+                    <div class="sportsmagazine-login-form">
+                        <h4>Login To Your Account</h4>
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <ul>
+                                <li><label for="Email">{{__('Email')}}</label><input type="email" name="email" value="old('email')" required autofocus autocomplete="username"></li>
+                                <li><label for="Email">{{__('Password')}}</label><input type="password" name="password" required autocomplete="current-password">
+                                    <!-- Remember Me -->
+                                    <div class="block mt-4">
+                                        <label for="remember_me" class="inline-flex items-center">
+                                            <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
+                                            <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                                        </label>
+                                    </div>
+                                    @if (Route::has('password.request'))
+                                    <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                                        {{ __('Forgot your password?') }}
+                                    </a>
+                                    @endif
+
+                                </li>
+                                <li>
+                                    <label class="submit-border"><input type="submit" value="{{ __('Log in') }}"><span></span></label>
+                                </li>
+                            </ul>
+                        </form>
+                        <ul class="sportsmagazine-login-social">
+                            <li><a href="https://www.facebook.com/" class="color-one"><i class="fa fa-facebook"></i>facebook</a></li>
+                            <li><a href="https://twitter.com/login" class="color-two"><i class="fa fa-twitter"></i>Twitter</a></li>
+                            <li><a href="https://plus.google.com/" class="color-three"><i class="fa fa-google-plus"></i>Google+</a></li>
+                            <li><a href="https://www.instagram.com/accounts/login/" class="color-four"><i class="fa fa-instagram"></i>Instagram</a></li>
+                        </ul>
+                        <span>Not A Member Yet ?<a href="#"> Sign - Up Now !</a></span>
+                    </div>
+                </div>
+                <!-- <div class="col-md-6">
+                            <div class="sportsmagazine-login-form sportsmagazine-register-form">
+                                <h4>Register Your Account Now</h4>
+                                <form>
+                                    <ul>
+                                        <li><label class="form-label">Your Email</label><input type="text" value="Enter here" onblur="if(this.value == '') { this.value ='Enter here'; }" onfocus="if(this.value =='Enter here') { this.value = ''; }"></li>
+                                        <li><label class="form-label">Password</label><input type="text" value="Enter here" onblur="if(this.value == '') { this.value ='Enter here'; }" onfocus="if(this.value =='Enter here') { this.value = ''; }"></li>
+                                        <li><label class="form-label">Confirm Password</label><input type="text" value="Enter here" onblur="if(this.value == '') { this.value ='Enter here'; }" onfocus="if(this.value =='Enter here') { this.value = ''; }"></li>
+                                        <li>
+                                            <label class="submit-border"><input type="submit" value="Sign In"><span></span></label>
+                                        </li>
+                                    </ul>
+                                </form>
+                            </div>
+                        </div> -->
+
+            </div>
         </div>
+    </div>
+    <!--// Main Section \\-->
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</div>
+<!--// Main Content \\-->
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\Admin\BracketDisplayController;
+use App\Http\Controllers\Admin\GameController;
+use App\Http\Controllers\Admin\LiveScoreController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ScheduleController;
+use App\Http\Controllers\Admin\TeamController;
+use App\Http\Controllers\Admin\PlayerController;
+use App\Http\Controllers\Admin\StandingController;
+use App\Http\Controllers\Admin\TicketController;
+use App\Http\Controllers\Admin\TicketTypeController;
+use App\Http\Controllers\Admin\TournamentController;
+use App\Http\Controllers\Admin\VenueController;
+use App\Http\Controllers\Admin\RefereeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,8 +28,38 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.index');
 });
+
+Route::get('create', [ScheduleController::class, 'create'])->name('create');
+Route::get('editschedule/{id}', [ScheduleController::class, 'editschedule'])->name('editschedule');
+Route::get('view', [ScheduleController::class, 'view'])->name('view');
+Route::post('store', [ScheduleController::class, 'store'])->name('store');
+Route::put('update/{id}', [ScheduleController::class, 'update'])->name('update');
+Route::post('destroy', [ScheduleController::class, 'destroy'])->name('destroy');
+
+Route::get('team_create', [TeamController::class, 'team_create'])->name('team_create');
+Route::get('team_edit', [TeamController::class, 'team_edit'])->name('team_edit');
+Route::get('team_view', [TeamController::class, 'team_view'])->name('team_view');
+Route::post('post_team', [TeamController::class, 'post_team'])->name('post_team');
+
+// Player route
+Route::get('player_create', [PlayerController::class, 'player_create'])->name('player_create');
+Route::get('player_edit', [PlayerController::class, 'player_edit'])->name('player_edit');
+Route::get('player_view', [PlayerController::class, 'player_view'])->name('player_view');
+Route::post('post_player', [PlayerController::class, 'post_player'])->name('post_player');
+
+Route::resource('referees', RefereeController::class);
+Route::resource('venues', VenueController::class);
+Route::resource('tickets', TicketController::class);
+Route::resource('games', GameController::class);
+Route::resource('standings', StandingController::class);
+Route::resource('livescores', LiveScoreController::class);
+Route::resource('brackets', BracketDisplayController::class);
+Route::resource('seats', TicketTypeController::class);
+Route::resource('tournaments', TournamentController::class);
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -28,4 +71,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
